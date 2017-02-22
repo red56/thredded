982e49215413db4b263b8c30877082d4cc317fb9
Tim Diggins <tim@red56.co.uk>
2017-02-22 11:25:37 +0000

    adding spec to double check

--- spec/models/thredded/user_detail_spec.rb
+++ spec/models/thredded/user_detail_spec.rb
@@ -19,4 +19,34 @@ module Thredded
       expect(user_details.reload.topics_count).to eq(1)
     end
   end
+
+  describe UserDetail, 'destroying' do
+    let!(:post) { create(:post, user: user_detail.user) }
+    let(:user) { create(:user) }
+    let(:user_detail) { create(:user_detail, user: user) }
+
+    it "destroying user doesn't delete user's posts but nulls them" do
+      expect { user.destroy }.not_to change { Thredded::Post.count }
+      expect(post.reload.user).to eq(nil)
+      expect(post.reload.user_id).to eq(nil)
+    end
+
+    it "destroying user deletes user's user detail" do
+      expect { user.destroy }.to change { Thredded::UserDetail.count }.by(-1)
+    end
+
+    it "destroying user detail doesn't delete user's posts" do
+      expect { user_detail.destroy }.not_to change { Thredded::Post.count }
+    end
+
+    describe 'also checking deletion' do
+      it "deleting user detail doesn't delete user's posts" do
+        expect { user.delete }.not_to change { Thredded::Post.count }
+      end
+
+      it "deleting user detail doesn't delete user's posts" do
+        expect { user.delete }.not_to change { Thredded::Post.count }
+      end
+    end
+  end
 end
